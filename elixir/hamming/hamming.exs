b87b6910117fa6f0301e75f3447c8fe8246fddfa
Mathew Button <mat@pushbutton.xyz>
2020-10-18 21:53:37 +1100

    Finished Hamming exercism in elixir

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -8,6 +8,22 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
+
+  def hamming_distance(strand1, strand2) when length(strand1) == length(strand2) do
+   {:ok, find_difference(strand1, strand2, 0)}
+  end
+
+  defp find_difference([strand1_head | strand1_tail], [strand2_head | strand2_tail], count) do
+    difference = if strand1_head != strand2_head, do: 1, else: 0
+    find_difference(strand1_tail, strand2_tail, count + difference)
+  end
+
+  defp find_difference([], [], count) do
+    count
+  end
+
+  @spec hamming_distance([char], [char]) :: non_neg_integer
   def hamming_distance(strand1, strand2) do
+   {:error, "Lists must be the same length"}
   end
 end
